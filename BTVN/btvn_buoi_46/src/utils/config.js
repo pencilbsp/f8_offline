64d652115403f955aedee0d4e28c1c2d1e8c19c3
pencildev <pencil.bsp@gmail.com>
2023-11-19 00:25:58 +0700

    Tao du an bai tap buoi 46

---
+++ BTVN/btvn_buoi_46/src/utils/config.js
@@ -0,0 +1,8 @@
+export const PRODUCT_PAGE_LIMIT = 12;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const ACTION_STATE = {
+  IDLE: "idle",
+  LOADING: "loading",
+  SUCCEEDED: "succeeded",
+  FAILED: "failed",
+};
